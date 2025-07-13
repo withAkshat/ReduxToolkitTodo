2cab3704cd98a290cbc924253f211dce4ab62aa0
Akshat Bamania <akshat.bamania23@gmail.com>
2025-07-13 17:15:46 +0530

    reducers logic

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoId } from "@reduxjs/toolkit"
+
+const initialState = { 
+    todos: [{id:1, text: "hello world!;"}] 
+}
+
+export const todoSlice = createSlice( {
+    name: 'todo',
+    initialState,
+    reducers:{
+        addTodo: (state, action)=>{
+            const todo = {
+                id: nanoId(),
+                text: action.payload
+            }
+
+            state.todos.push(todo);
+
+        },
+        
+        removeTodo: (state, action)=>{
+            
+            state.todos = state.todos.filter( (todo)=> todo.id != action.payload );
+
+        },
+
+        updateTodo: (state, action)=>{
+           state.todos = state.todos.map( (todo)=>  todo.id === action.payload.id? {...todo,text: action.payload.text} : todo )
+        }
+    }
+} )
+
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+export default todoSlice.reducer;
